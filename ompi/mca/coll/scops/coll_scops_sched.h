f3bf0d5adc8b0bbb7ddd1eb088847ed28f5a77c8
Alex Anenkov <anenkov.ru@gmail.com>
2019-07-27 21:14:10 +0700

    scops_sched_send, scops_sched_recv

--- ompi/mca/coll/scops/coll_scops_sched.h
+++ ompi/mca/coll/scops/coll_scops_sched.h
@@ -8,14 +8,36 @@
 #ifndef MCA_COLL_SCOPS_EXPORT_H
 #define MCA_COLL_SCOPS_EXPORT_H
 
+struct scops_sched_send {
+    const void *buf;
+    int count;
+    MPI_Datatype datatype;
+    int dest;
+    int tag;
+    MPI_Comm comm;
+    struct ompi_request *sreq;
+};
+
+struct scops_sched_recv {
+    void *buf;
+    int count;
+    MPI_Datatype datatype;
+    int source;
+    int tag;
+    MPI_Comm comm;
+    struct ompi_request *rreq;
+};
+
+
+
 struct scops_sched_entry {
     bool is_barrier;
-    // union {
-    //     struct scops_sched_send;
-    // } op;
+    union {
+        struct scops_sched_send send;
+        struct scops_sched_recv recv;
+    } op;
     struct scops_sched_entry *next, *prev;
 };
-typedef struct scops_sched_entry scops_sched_entry;
 
 
 
